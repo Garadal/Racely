de03e70394e5ea7eca99b60bbfe85488329921f9
Karol Biczuk <karolbiczuk@gmail.com>
2023-05-18 19:33:11 +0200

    Initial commit

---
+++ src/main/java/com/Racely/RacelyApplication.java
@@ -0,0 +1,13 @@
+package com.Racely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacelyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Racely/RacelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Racely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
